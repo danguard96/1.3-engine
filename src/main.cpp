fedec08f896ebc1886b4fb91d2471f5bcdcc759e
afBejarano <31251331+afBejarano@users.noreply.github.com>
2025-09-16 22:03:53 -0500

    test

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,19 @@
 //
 
 #include <iostream>
+#include <GLFW/glfw3.h>
+#include <lightweightvk/lvk/LVK.h>
 
 int main (int argc, char *argv[]) {
+    glfwInit();
+    int width {800};
+    int height {600};
+    GLFWwindow *window = lvk::initWindow("VKEngine", width, height, false);
+    std::unique_ptr<lvk::IContext> ctx = lvk::createVulkanContextWithSwapchain(window, width, height, {});
+    while (!glfwWindowShouldClose(window)) {
+        glfwPollEvents();
+    }
+    glfwDestroyWindow(window);
+    glfwTerminate();
     std::cout << "Hello World!\n";
 }
